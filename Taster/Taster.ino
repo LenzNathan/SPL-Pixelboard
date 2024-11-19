e1ee8cf184de5d39c5a0a1c02ee655513a2f47db
Nathan Lenz <nathan@freelenzer.at>
2024-11-19 14:26:11 +0100

    kommentare zum neuen code

--- Taster/Taster.ino
+++ Taster/Taster.ino
@@ -1,60 +1,78 @@
-#include <Arduino.h>
+class EntprellterTaster {
+  private:
+    int pin;
+    bool entprellterZustand;
+    unsigned long letzteAenderung;   
+    unsigned long druckBeginn;
+    bool gedruecktGemeldet; // für nicht dauerhaft auslösung nachdem die bedingungen für den druck wahr sind
+    static const unsigned long entprellZeit = 50; // 50 ms Entprellzeit
+    static const unsigned long langeDruckZeit = 1000; // 1 Sekunde für langen Druck
 
-class Taster {
-private:
-  int pin;
-  unsigned long entprellZeit = 50;
-  unsigned long letzteFlanke = 0;
-  bool letzterZustand = false;
-  bool entprellterZustand = false;
-public:
-  Taster(int pin, unsigned long entprellZeit) {
-    this->pin = pin;
-    this->entprellZeit = entprellZeit;
-    pinMode(pin, INPUT_PULLUP);
-    letzterZustand = !digitalRead(pin);  // initiales Lesen des inversen Zustands
-  }
+  public:
+    EntprellterTaster(int tasterPin) //Konstruktor
+      : pin(tasterPin), entprellterZustand(false), letzteAenderung(0), druckBeginn(0), gedruecktGemeldet(true) { // setzt die default werte wie sonst standardmäßig untereinander
+      pinMode(pin, INPUT_PULLUP);
+    }
 
-  void aktualisieren() {
-    unsigned long jetzt = millis();
-    bool zustand = !digitalRead(pin);  // inverse Logik des Tasters
+    void aktualisiere() {
+      bool aktuellerZustand = !digitalRead(pin); // Negation für negative Logik - ab hier positive logik
+      unsigned long aktuelleZeit = millis();
 
-    if (jetzt - letzteFlanke > entprellZeit) {
-      entprellterZustand = zustand;
-      if (letzterZustand != zustand) {
-        letzteFlanke = jetzt;
+      if (aktuellerZustand != entprellterZustand && (aktuelleZeit - letzteAenderung) > entprellZeit) { // entprellen logik: nur bei zustandsänderung && prellzeit überschritten
+        entprellterZustand = aktuellerZustand; // zustand aktualisieren (die änderung ist legitim - oben geprüft)
+        letzteAenderung = aktuelleZeit; // aktualisieren da geändert wurde
+
+        if (entprellterZustand) { // wenn der taster an is, dann 
+          druckBeginn = aktuelleZeit; // is er seit jetzt an
+          gedruecktGemeldet = false;  // und wir dürfen erneut melden
+        }
       }
     }
-    letzterZustand = zustand;
-  }
 
-  bool
-  istGedrueckt() {
-    return entprellterZustand;
-  }
-};
+    bool istGedrueckt() {
+      return entprellterZustand;
+    }
 
+    bool wurdeGedrueckt() {
+      if (!entprellterZustand && !gedruecktGemeldet) { //erst wenn wieder losgelassen wird
+        if (millis() - druckBeginn < langeDruckZeit) {
+          gedruecktGemeldet = true; // nur einmal melden (kommt in der bedingung ja auch vor)
+          return true;
+        }
+      } // else
+      return false;
+    }
 
+    bool wurdeLangeGedrueckt() {
+      if (entprellterZustand && !gedruecktGemeldet) {
+        if (millis() - druckBeginn >= langeDruckZeit) { // wie oben nur dass wir länger als langedruckzeit halten müssen
+          gedruecktGemeldet = true;
+          return true;
+        }
+      }
+      return false;
+    }
+}; // sehr toll!
 
-//----------------------------------------------------------------------------------------------------------------
-
-
-const int TASTER_PIN = 8;  // Beispiel-Pin
-
-Taster meinTaster(TASTER_PIN, 50);  // Beispiel: Pin-Nummer und Entprellzeit angeben
+// Testprogramm
+const int TASTER_PIN = 2;
+EntprellterTaster taster(TASTER_PIN);
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starte Programm...");
+  Serial.begin(9600);
+  Serial.println("Taster-Test gestartet");
 }
 
 void loop() {
-  meinTaster.aktualisieren();
-  bool zustand = meinTaster.istGedrueckt();
+  taster.aktualisiere();
 
-  static int anzahlKlick = 0;
+  if (taster.wurdeGedrueckt()) {
+    Serial.println("Taster wurde kurz gedrückt");
+  }
 
-  Serial.println(zustand);
+  if (taster.wurdeLangeGedrueckt()) {
+    Serial.println("Taster wurde lange gedrückt");
+  }
 
-  delay(10);  // Kleine Verzögerung für Stabilität der Schleife
-}
+  
+} 
