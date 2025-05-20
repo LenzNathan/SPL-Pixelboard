2a652cab2f9538b968279d0cbe52ece969579cbc
ArnoutShithead <arnwens05@gmail.com>
2025-05-20 14:55:56 +0200

    TimeServer_update
    Bitte testen

---
+++ sketch_sep24b/sketch_sep24b.ino
@@ -0,0 +1,69 @@
+#include <WiFi.h>
+#include <time.h>
+
+// WLAN-Zugangsdaten
+const char* wlanName = "Selbst eingeben :)";
+const char* wlanPasswort = "Selbst eingeben :)";
+
+// NTP-Server und Zeitzone
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffsetInSeconds = 3600;      // GMT+1 (MEZ)
+const int daylightOffsetInSeconds = 3600;  // Sommerzeit
+
+// Initialisierung
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  verbindeMitWLAN();
+  konfiguriereZeit();
+
+  Serial.println("Initialisierung abgeschlossen.");
+}
+
+// Hauptloop
+void loop() {
+  zeigeAktuelleZeit();
+  delay(10000);  // Alle 10 Sekunden aktualisieren
+}
+
+// Verbindung mit dem WLAN herstellen
+void verbindeMitWLAN() {
+  Serial.print("Verbinde mit WLAN: ");
+  Serial.println(wlanName);
+
+  WiFi.begin(wlanName, wlanPasswort);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWLAN verbunden.");
+  Serial.print("IP-Adresse: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Zeit vom NTP-Server konfigurieren
+void konfiguriereZeit() {
+  configTime(gmtOffsetInSeconds, daylightOffsetInSeconds, ntpServer);
+  Serial.println("Zeitkonfiguration abgeschlossen.");
+}
+
+// Aktuelle Zeit ausgeben
+void zeigeAktuelleZeit() {
+  struct tm zeitinfo;
+
+  if (!getLocalTime(&zeitinfo)) {
+    Serial.println("Fehler beim Abrufen der Zeit.");
+    return;
+  }
+
+  char zeichenkette[64];
+  strftime(zeichenkette, sizeof(zeichenkette),
+           "%A, %d.%m.%Y %H:%M:%S", &zeitinfo);
+
+  Serial.print("Aktuelle Zeit: ");
+  Serial.println(zeichenkette);
+}
+K
