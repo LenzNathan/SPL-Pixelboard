2bd5f79b191a1118407004e2ef3c2db499179def
ArnoutShithead <124769421+ArnoutShithead@users.noreply.github.com>
2025-05-27 16:44:19 +0200

    DHT22 Anzeige

--- LED-Panel-Fastled-test/LED-Panel-Fastled-test.ino
+++ LED-Panel-Fastled-test/LED-Panel-Fastled-test.ino
@@ -1,68 +1,104 @@
 #include <FastLED.h>
-
 #include <LEDMatrix.h>
-#include <LEDText.h>
-#include <FontMatrise.h>
+#include <DHT.h>
 
-// Change the next 6 defines to match your matrix type and size
+#define WIDTH        32
+#define HEIGHT       8
+#define CHIPSET      WS2812B
+#define COLOR_ORDER  GRB
+#define BRIGHTNESS   50
 
-#define LED_PIN 25
-#define COLOR_ORDER GRB
-#define CHIPSET WS2812B
+#define TEMP_PIN     26
+#define HUM_PIN      25
 
-#define MATRIX_WIDTH -32
-#define MATRIX_HEIGHT -8
-#define MATRIX_TYPE VERTICAL_ZIGZAG_MATRIX
+#define DHTPIN       14
+#define DHTTYPE      DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
-int h = 0;
-int m = 0;
+// Matrix-Buffer
+CRGB ledsTemp[WIDTH * HEIGHT];
+CRGB ledsHum[WIDTH * HEIGHT];
 
-cLEDMatrix<-MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
+// Zahlendarstellung 3x5 Pixel
+const byte digits[10][5] = {
+  {0b111, 0b101, 0b101, 0b101, 0b111}, // 0
+  {0b010, 0b110, 0b010, 0b010, 0b111}, // 1
+  {0b111, 0b001, 0b111, 0b100, 0b111}, // 2
+  {0b111, 0b001, 0b111, 0b001, 0b111}, // 3
+  {0b101, 0b101, 0b111, 0b001, 0b001}, // 4
+  {0b111, 0b100, 0b111, 0b001, 0b111}, // 5
+  {0b111, 0b100, 0b111, 0b101, 0b111}, // 6
+  {0b111, 0b001, 0b010, 0b100, 0b100}, // 7
+  {0b111, 0b101, 0b111, 0b101, 0b111}, // 8
+  {0b111, 0b101, 0b111, 0b001, 0b111}  // 9
+};
 
-cLEDText ClockTime;
+// Zeichenfunktion: 1 Ziffer
+void drawDigit(CRGB* leds, int digit, int xOffset, int yOffset, CRGB color) {
+  for (int y = 0; y < 5; y++) {
+    for (int x = 0; x < 3; x++) {
+      if (digits[digit][y] & (1 << (2 - x))) {
+        leds[(y + yOffset) * WIDTH + (x + xOffset)] = color;
+      }
+    }
+  }
+}
 
-unsigned char TxtDemo[] = { "   :  " };
+// Löscht den Puffer
+void clearLeds(CRGB* leds) {
+  for (int i = 0; i < WIDTH * HEIGHT; i++) {
+    leds[i] = CRGB::Black;
+  }
+}
 
+// Anzeige: Zwei Ziffern + ° oder %
+void showValue(CRGB* leds, int value, bool isTemp) {
+  clearLeds(leds);
 
-void setup() {
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds[0], leds.Size());
-  FastLED.setBrightness(10);
-  FastLED.clear(true);
-  delay(500);
-  FastLED.show();
+  int v1 = value / 10;
+  int v2 = value % 10;
 
-  ClockTime.SetFont(MatriseFontData);
-  ClockTime.Init(&leds, leds.Width(), ClockTime.FontHeight() + 1, 0, 0);
-  ClockTime.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
-  ClockTime.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0x00, 0xff);
+  drawDigit(leds, v1, 1, 1, isTemp ? CRGB::Red : CRGB::Blue);
+  drawDigit(leds, v2, 6, 1, isTemp ? CRGB::Red : CRGB::Blue);  // Abstand verbessert
+
+  // Symbole setzen
+  if (isTemp) {
+    leds[1] = CRGB::Red;
+    leds[WIDTH * 0 + 11] = CRGB::Red;  // ° weiter rechts
+  } else {
+    leds[WIDTH * 0 + 11] = CRGB::Blue; // % Punkt
+    leds[WIDTH * 1 + 10] = CRGB::Blue;
+  }
 }
 
 
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+  FastLED.addLeds<CHIPSET, TEMP_PIN, COLOR_ORDER>(ledsTemp, WIDTH * HEIGHT);
+  FastLED.addLeds<CHIPSET, HUM_PIN, COLOR_ORDER>(ledsHum, WIDTH * HEIGHT);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
 void loop() {
-  ClockTime.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
 
-  ClockTime.UpdateText();  //Anzeigen
-  for (int i = 0; i < 5; i++) {
-    ClockTime.UpdateText();  //um eins verschieben, weil 6*5 = 30 pixel, von 32 Pixel macht 1 Pixel Abstand / Seite
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("Sensorfehler!");
+    return;
   }
 
-  String hours = String(h);
-  if (h < 10) {
-    hours = "0" + hours;
-  }
-  String minutes = String(m);
-  if (m < 10) {
-    minutes = "0" + minutes;
-  }
-  for (int i = 0; i < 2; i++) {
-    TxtDemo[i + 1] = hours[i];
-  }
-  for (int i = 0; i < 2; i++) {
-    TxtDemo[i + 4] = minutes[i];
-  }
+  Serial.print("Temperatur: ");
+  Serial.print(temp);
+  Serial.print(" °C, Luftfeuchtigkeit: ");
+  Serial.print(hum);
+  Serial.println(" %");
+
+  showValue(ledsTemp, (int)temp, true);
+  showValue(ledsHum, (int)hum, false);
 
   FastLED.show();
-  delay(1000);
-  h++;
-  m++;
+  delay(2000);
 }
